f85ded5913d74977734b259a66d8a1427a4f6467
Andrew J Ortiz <112707094+andrew-j-ortiz@users.noreply.github.com>
2025-02-25 16:27:10 -0500

    Add functions for every operator

---
+++ calculator.js
@@ -0,0 +1,18 @@
+// operator functions
+// take two numbers and either add, substract, multiply, or divide
+
+function add(intFirstNumber, intSecondNumber) {
+    return intFirstNumber + intSecondNumber;
+}
+
+function substract(intFirstNumber, intSecondNumber) {
+    return intFirstNumber - intSecondNumber
+}
+
+function multiply(intFirstNumber, intSecondNumber) {
+    return intFirstNumber * intSecondNumber
+}
+
+function divide(intFirstNumber, intSecondNumber) {
+    return intFirstNumber / intSecondNumber
+}
